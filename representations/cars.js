251f33576f9f64dc29f2225980ddfc8020d19583
codenoobies <kevindubidaam@gmail.com>
2023-03-16 12:20:05 +0700

    Finished Read All Cars Feature

--- representations/cars.js
+++ representations/cars.js
@@ -4,6 +4,7 @@ const path = require("path");
 // Class
 const APIError = require(path.join(__dirname, "../class/APIerror.js"));
 const APIResponse = require(path.join(__dirname, "../class/APIResponse.js"));
+const QueryConstructor = require(path.join(__dirname, "../class/QueryConstructor.js"));
 
 // Models
 const cars = require(path.join(__dirname, "../resources/cars.js"));
@@ -21,4 +22,14 @@ exports.postCar = async function (req, res, next) {
     const { _doc } = await cars.create({ sellerId: seller._id, ...body });
 
     return res.status(201).json(new APIResponse(200, "success", "Car Successfully Created", _doc));
+};
+
+exports.getCars = async function (req, res, next) {
+    const { query } = req;
+
+    const { mongoQuery } = new QueryConstructor(cars, query).filterOrGet();
+
+    const foundCars = await mongoQuery;
+
+    res.status(200).json(new APIResponse(200, "success", "Data Successfully Fetched", foundCars));
 }
