cc33aedea6a895cd822d077b9f7036d3c330982a
codenoobies <kevindubidaam@gmail.com>
2023-03-17 15:47:29 +0700

    Finished Update User Data

--- representations/users.js
+++ representations/users.js
@@ -12,16 +12,12 @@ const APIResponse = require(path.join(__dirname, "../class/APIResponse.js"));
 const validateBody = require(path.join(__dirname, "../methods/validateBody.js"));
 
 exports.updateMyUser = async (req, res, next) => {
-    const { body } = req;
+    const { body, user } = req;
 
     if (!Object.keys(body).length) return next(new APIError(400, "Please attach a data"));
 
-    if (!validateBody(body, 3, next)) return;
-
     const { name, email } = body;
 
-    req.user = { ...req.user, name, email }
-    console.log(req.user);
-
-    res.status(200).json({ message: "ok" });
+    const newData = await users.findOneAndUpdate({ _id: user._id }, { name, email }, { new: true }).lean();
+    res.status(200).json({ message: "ok", data: newData });
 };
