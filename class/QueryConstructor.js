a0b659fef15d298fea9d22a868f3df158455ec6b
codenoobies <kevindubidaam@gmail.com>
2023-03-19 17:09:48 +0700

    Finished Search Cars by locations Radius

--- class/QueryConstructor.js
+++ class/QueryConstructor.js
@@ -4,11 +4,19 @@ class APiData {
         this.urlQuery = urlQuery;
     }
 
+    #constructGeospatial(query, coordinates) {
+        query.location = {
+            $geoWithin: {
+                $centerSphere: [coordinates, 12 / 6378]
+            }
+        };
+    }
+
     filterOrGet() {
         if (this.urlQuery) {
             const query = this.urlQuery;
             const excludedQuery = ["page", "sort", "limit", "fields"];
-            const includedQuery = ["name", "model", "bodyType", "ATMT", "brand", "price", "cc"];
+            const includedQuery = ["name", "model", "bodyType", "ATMT", "brand", "price", "cc", "coordinates"];
             excludedQuery.forEach((value) => delete query[value]);
 
             Object.keys(query).forEach((value) => {
@@ -20,6 +28,16 @@ class APiData {
                 if (!includedQuery.includes(value)) delete query[value];
             });
 
+            if ("coordinates" in query) {
+                const [longitude, latitude] = query.coordinates.split(",");
+
+                if (longitude && latitude) {
+                    this.#constructGeospatial(query, [longitude * 1, latitude * 1])
+                    delete query["coordinates"];
+                };
+
+            }
+
             this.mongoQuery = this.mongoQuery.find({ ...query, status: "Posted" });
 
             return this;
@@ -29,6 +47,7 @@ class APiData {
 
         return this;
     }
+
 
     pagination() {
         const [page, itemsPerPage] = [(this.urlQuery.page * 1 || 1) - 1, this.urlQuery.itemsPerPage * 1 || 10];
