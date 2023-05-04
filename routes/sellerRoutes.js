838c002ec566730eb84b959bf5c6a63ff931eb95
codenoobies <kevindubidaam@gmail.com>
2023-05-04 10:56:38 +0700

    Added get Seller Data

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -1,5 +1,6 @@
 // Core Modules
 const path = require('path');
+const { isObjectId } = require('../auth/auth');
 
 // Auth
 const { registerSeller, loginSeller } = require(path.join(__dirname, "../auth/sellers.js"));
@@ -10,7 +11,7 @@ const { isSeller } = require(path.join(__dirname, "../auth/SellersAuth.js"));
 const asyncHandler = require(path.join(__dirname, "../error/AsyncHandler.js"))
 
 // Methods
-const { updateMySeller, deleteMySeller } = require(path.join(__dirname, "../representations/sellers.js"));
+const { updateMySeller, deleteMySeller, getSeller } = require(path.join(__dirname, "../representations/sellers.js"));
 
 // Image Process
 const processImage = require(path.join(__dirname, "../methods/single-process/processImage.js"));
@@ -23,6 +24,8 @@ const router = require("express").Router();
 const transactionsRoute = require(path.join(__dirname, "./transactionRoutes.js"));
 const bookingRoutes = require(path.join(__dirname, "./bookingRoutes.js"));
 
+router.route("/:_id").get(isObjectId, asyncHandler(getSeller));
+
 router.route("/register").post(asyncHandler(isLoggedIn), asyncHandler(registerSeller));
 router.route("/login").post(asyncHandler(isLoggedIn), asyncHandler(loginSeller));
 
@@ -30,6 +33,7 @@ router.use(asyncHandler(isLoggedIn), asyncHandler(isSeller));
 router.use("/myTransactions", transactionsRoute); //! Transactions Router
 router.use("/myBookings", bookingRoutes); //! Transactions Router
 
+// Manipulate Seller Data
 router.route("/me").patch(upload.single("photo"), asyncHandler(processImage("sellers")), asyncHandler(updateMySeller));
 router.route("/deleteme").delete(asyncHandler(deleteMySeller));
 
