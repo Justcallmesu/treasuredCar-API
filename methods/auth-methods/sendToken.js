f34d9c250297bbe3d03083546947b270d1ad30a6
codenoobies <kevindubidaam@gmail.com>
2023-05-06 00:19:26 +0700

    fixed cookies

--- methods/auth-methods/sendToken.js
+++ methods/auth-methods/sendToken.js
@@ -34,11 +34,11 @@ async function sendToken(req, res, email, data, role = "user") {
         {
             maxAge: refreshCookieAge,
             httpOnly: true,
-            secure: req.protocol === "https" ? true : false,
+            secure: true,
             sameSite: "none"
         })
         .cookie(`${role}Token`, token, {
-            secure: req.protocol === "https" ? true : false,
+            secure: true,
             sameSite: "none"
         })
         .status(data.statusCode).json(data);
